824db0f9fef5ee724a7a5c8df4587ca7ff70c014
Dagaflux <dagaflux>
2017-08-01 21:51:02 +0200

    Init du projet

---
+++ Assets/MonScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonScript : MonoBehaviour {
+
+	string tamere = "MANGE TA GLACE";
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("La boite à flûte");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter () {
+		
+	}
+}
